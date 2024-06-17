f72b7bb887060d459d264f61466a0e9377a5b5cd
kyungho1224 <dev.kyungho.kim@gmail.com>
2024-06-17 15:06:44 +0900

    #2-BottomNavigationBar 구현

---
+++ lib/screen/sub_screen2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SubScreen2 extends StatelessWidget {
+  const SubScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('sub 2'),
+    );
+  }
+}
---
+++ lib/screen/sub_screen4.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SubScreen4 extends StatelessWidget {
+  const SubScreen4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('sub 4'),
+    );
+  }
+}
